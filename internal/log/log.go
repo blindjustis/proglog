ef3d6fc07ec04520d7fcdac8952304f65bf416bd
David Hewitt <david.hewitt@incdev.com>
2021-07-08 07:23:36 +0100

    start log impl

---
+++ internal/log/log.go
@@ -0,0 +1,76 @@
+package log
+
+import (
+	"fmt"
+	"io"
+	"io/util"
+	"os"
+	"path"
+	"sort"
+	"strconv"
+	"strings"
+	"sync"
+
+	api "github.com/blindjustis/proglog/api/log_v1"
+)
+
+type Log struct{
+	mu sync.RwMutex
+
+	Dir string
+	Config Config
+	activeSegment *segment
+	segments []*segment
+}
+
+func NewLog(dir string, c Config) (*log error){
+	if c.Segment.MaxStoreBytes == 0{
+		c.Segment.MaxStoreBytes = 1024
+	}
+	if c.Segment.MaxIndexBytes == 0 {
+		c.Segment.MaxIndexBytes = 1024
+	}
+
+	l:= &Log{
+		Dir: dir,
+		Config: c,
+	}
+
+	return l,l.setup()
+}
+
+func (l *Log) setup() error {
+	files,err := ioutil.ReadDir(l.Dir)
+	if ( err != nil)
+	{
+		return err
+	}
+
+	var baseOffsets []uint64
+	for _, file := range files {
+		offStr := strings.TrimSuffix(
+			file.Name(),
+			path.Ext(file.Name()),
+		)
+		off,_ := strconv.ParseUint(offStr,10,0)
+		baseOffsets = append(baseOffsets, off)
+	}
+
+	sort.Slice(baseOffsets,func(i,j int) bool{
+		return baseOffsets[i] < baseOffsets[j]
+	})
+
+	for i := 0 i < len(baseOffsets); i++{
+		if err = l.newSegment(baseOffsets[i]); err != nil{
+			return err
+		}
+		i++;
+	}
+
+	if l.segments == nil {
+		if err = l.newSegment(l.Config.Segment.InitialOffset); err != nil{
+			return err
+		}
+	}
+	return nil
+}
